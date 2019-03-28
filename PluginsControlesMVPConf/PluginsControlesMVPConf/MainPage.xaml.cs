4091aa4e9a2802a06ed7ff205706599aa1bd98e0
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-03-28 09:51:30 -0300

    :tada: Subindo primeira versão pro GIT

---
+++ PluginsControlesMVPConf/PluginsControlesMVPConf/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PluginsControlesMVPConf
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void ffImageLoading_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PluginsControlesMVPConf.Views.FFImageLoading());
+        }
+    }
+}

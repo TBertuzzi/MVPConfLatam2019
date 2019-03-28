c215e720abdf204e435cb13c0667b36fdc12892a
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-03-28 10:10:44 -0300

    :sparkles: Adicionado Notificações

--- PluginsControlesMVPConf/PluginsControlesMVPConf/MainPage.xaml.cs
+++ PluginsControlesMVPConf/PluginsControlesMVPConf/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.LocalNotifications;
 using Xamarin.Forms;
 
 namespace PluginsControlesMVPConf
@@ -17,6 +18,12 @@ namespace PluginsControlesMVPConf
         public async void ffImageLoading_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new PluginsControlesMVPConf.Views.FFImageLoading());
+        }
+
+        public void localNotifications_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Testando as Notificações", "Agora va ler mais artigos do Bertuzzi no Medium", 1, DateTime.Now.AddSeconds(10));
+            Application.Current.MainPage.DisplayAlert("Atenção", "Feche o Aplicativo a notificação vai aparecer em 10 Segundos", "ok");
         }
     }
 }
